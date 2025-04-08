bd6391edb5b5e110cb8a7e509d73664581d6f462
ChaiyoKung <kananek45@gmail.com>
2025-04-08 17:11:14 +0700

    style: format code

--- src/MyStack.ts
+++ src/MyStack.ts
@@ -1,77 +1,88 @@
-import { type CallbackProvider, Terrakit, TerrakitController, type TerrakitOptions, TerrakitStack } from "terrakit";
+import {
+  type CallbackProvider,
+  Terrakit,
+  TerrakitController,
+  type TerrakitOptions,
+  TerrakitStack,
+} from "terrakit";
 import { Construct } from "constructs";
-import type { SetRequired } from 'type-fest';
+import type { SetRequired } from "type-fest";
 import { ResourceGroup } from "@cdktf/provider-azurerm/lib/resource-group";
 import { StorageAccount } from "@cdktf/provider-azurerm/lib/storage-account";
 
 export interface MyTerrakitStackConfig {
   identifier: {
-    env: 'prod';
-    slot: 'prod' | 'staging';
-    site: 'active' | 'dr';
+    env: "prod";
+    slot: "prod" | "staging";
+    site: "active" | "dr";
   };
   providers: {
     defaultAzureProvider: CallbackProvider;
   };
 }
 
-export const createController = (stack: TerrakitStack<MyTerrakitStackConfig>) => {
+export const createController = (
+  stack: TerrakitStack<MyTerrakitStackConfig>
+) => {
   return new TerrakitController(stack, stack.providers)
     .add({
-      id: 'aaa1',
+      id: "aaa1",
       type: ResourceGroup,
       config: ({ providers }) => ({
         provider: providers.defaultAzureProvider,
-        name: 'rg-' + 'aaa1',
-        location: 'eastus'
+        name: "rg-" + "aaa1",
+        location: "eastus",
       }),
     })
     .add({
-      id: 'aaa2',
+      id: "aaa2",
       type: StorageAccount,
       config: ({ providers, outputs }) => ({
         provider: providers.defaultAzureProvider,
-        name: 'sa' + 'aaa2',
+        name: "sa" + "aaa2",
         resourceGroupName: outputs.aaa1.name,
-        location: 'eastus',
-        accountReplicationType: 'LRS',
-        accountTier: 'Standard'
+        location: "eastus",
+        accountReplicationType: "LRS",
+        accountTier: "Standard",
       }),
     })
     .add({
-      id: 'aaa3',
-      if: stack.options.identifier.env === 'prod',
+      id: "aaa3",
+      if: stack.options.identifier.env === "prod",
       type: StorageAccount,
       config: ({ providers, outputs }) => ({
         provider: providers.defaultAzureProvider,
-        name: 'sa' + 'aaa3',
+        name: "sa" + "aaa3",
         resourceGroupName: outputs.aaa2.accessTier,
-        location: 'eastus',
-        accountReplicationType: 'LRS',
-        accountTier: 'Standard'
+        location: "eastus",
+        accountReplicationType: "LRS",
+        accountTier: "Standard",
       }),
     })
     .add({
-      id: 'aaa4',
+      id: "aaa4",
       type: StorageAccount,
       config: ({ providers, outputs }) => ({
         provider: providers.defaultAzureProvider,
-        name: 'sa' + 'aaa4',
-        resourceGroupName: outputs.aaa3?.name ?? 'default-rg',
-        location: 'eastus',
-        accountReplicationType: 'LRS',
-        accountTier: 'Standard',
+        name: "sa" + "aaa4",
+        resourceGroupName: outputs.aaa3?.name ?? "default-rg",
+        location: "eastus",
+        accountReplicationType: "LRS",
+        accountTier: "Standard",
       }),
     });
-
-}
+};
 
 export function createMyStack(
   scope: Construct,
-  options: SetRequired<TerrakitOptions<MyTerrakitStackConfig>, 'identifier' | 'providers'>
+  options: SetRequired<
+    TerrakitOptions<MyTerrakitStackConfig>,
+    "identifier" | "providers"
+  >
 ) {
-  const terrakitStack = new TerrakitStack<MyTerrakitStackConfig>(scope, options);
-  return new Terrakit(terrakitStack)
-    .setController(createController)
+  const terrakitStack = new TerrakitStack<MyTerrakitStackConfig>(
+    scope,
+    options
+  );
+  return new Terrakit(terrakitStack).setController(createController);
 }
-
--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,25 @@ import { AzurermProvider } from "@cdktf/provider-azurerm/lib/provider";
 const app = new App();
 const myStack = createMyStack(app, {
   identifier: {
-    env: 'prod',
-    slot: 'prod',
-    site: 'active'
+    env: "prod",
+    slot: "prod",
+    site: "active",
   },
   providers: {
-    defaultAzureProvider: (scope) => new AzurermProvider(scope, "azurerm_provider_default", {
-      // skipProviderRegistration: true,
-      resourceProviderRegistrations: 'core',
-      subscriptionId: '00000000-0000-0000-0000-000000000000',
-      features: [{}]
-    })
+    defaultAzureProvider: (scope) =>
+      new AzurermProvider(scope, "azurerm_provider_default", {
+        // skipProviderRegistration: true,
+        resourceProviderRegistrations: "core",
+        subscriptionId: "00000000-0000-0000-0000-000000000000",
+        features: [{}],
+      }),
   },
 })
   .overrideResources({
     aaa1: {
-      name: 'custom-rg'
-    }
+      name: "custom-rg",
+    },
   })
   .build();
 
-
 app.synth();
-
